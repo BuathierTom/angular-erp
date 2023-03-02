2158477c0bbc999c551c11904b89796dd722b18a
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2023-03-02 14:43:13 +0100

    Ajout d'un bouton Notify et d'un popUp avec

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
@@ -6,4 +6,7 @@ import { Product } from '../products';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent {}
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
