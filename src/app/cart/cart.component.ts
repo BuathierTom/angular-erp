d3c7e309b9395d571cf273efcd570719088fa80a
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2023-03-02 15:23:39 +0100

    Ajout d'un formulaire lors de l'achat

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,5 +10,21 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items = this.cartService.getItems();
-  constructor(private cartService: CartService) {}
+
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
