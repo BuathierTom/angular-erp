1ba48264caddf9345b08696d541f7e3ea617770c
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2023-03-02 14:26:24 +0100

    Update README.MD

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # TP_ERP
 
-##
+Site en HTML5 qui relie une liste de produit créé ultérieurement
+
+## Author
+
+- [@BuathierTom](https://www.github.com/BuathierTom)
+
+## Documentation
+
+Suivi d'un tuto qui provient de [Angular.io](https://angular.io/start).
+
+## License
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sq2jb2-d7cfkk)
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
-export class ProductAlertsComponent {
-
-}
+export class ProductAlertsComponent {}
